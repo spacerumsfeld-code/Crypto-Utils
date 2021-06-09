8d7a257e66a4887e771dfee30bf50fd34097063e
Nick Papadakis <nickfin2014@gmail.com>
2021-06-08 18:08:01 -0700

    added basic API route /w external API call for testing purposes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,15 @@
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+import axios from 'axios';
+
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    try {
+      const todo = await axios.get(url);
+      res.send(todo.data);
+    } catch (error) {
+      console.log(error.stack);
+    }
+  } else {
+    res.status(200).json({ name: 'John Doe' });
+  }
+};
