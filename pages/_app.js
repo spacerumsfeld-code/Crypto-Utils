5fd00280b43aee06b89f80285be27569c92f8e95
Nick Papadakis <nickfin2014@gmail.com>
2021-06-08 17:58:59 -0700

    added chakraUI provider to _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
