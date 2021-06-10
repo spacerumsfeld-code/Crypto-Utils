ea9cb7453d40628eebba56216aee24d4d4123de7
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 16:45:45 -0700

    adjusted icon sizing in footer

--- Components/FooterLink.js
+++ Components/FooterLink.js
@@ -2,7 +2,7 @@ import { Link, Icon } from '@chakra-ui/react';
 
 const FooterLink = ({ icon, href, label }) => (
   <Link display="inline-block" href={href} aria-label={label} isExternal>
-    <Icon as={icon} fontSize="xl" color="white.100" />
+    <Icon as={icon} fontSize="xl" boxSize="1.5em" color="white.100" />
   </Link>
 );
 
