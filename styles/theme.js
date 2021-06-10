6be493f4aefd17576dd1e1e13cb46d9ad39a876b
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 13:14:39 -0700

    added new font and color scheme in comments to incorporate into theme

--- styles/theme.js
+++ styles/theme.js
@@ -6,8 +6,38 @@ const theme = extendTheme({
     useSystemColorMode: true
   },
   fonts: {
-    body: 'Georgia'
+    body: 'Menlo'
   }
 });
 
 export default theme;
+
+
+/*
+#7aa6da
+yellow
+bg color #002451
+lighter yellow #ffeead
+light purple #ebbbff
+orange #ffc58f
+red negative tweets #d54e53
+light green positive tweets #d1f1a9
+
+/*
+button styling
+
+.button:hover, .button:focus {
+  color: #fff;
+  outline: 0;
+}
+
+.button-bitcoin, .button-take-snapshot {
+  border-color: #ffeead;
+  color: #fff;
+  background-image: -webkit-linear-gradient(45deg, #ffeead 50%, transparent 50%);
+  background-image: linear-gradient(45deg, #ebbbff, 50%, transparent 50%);
+  background-position: 100%;
+  background-size: 400%;
+  -webkit-transition: background 300ms ease-in-out;
+  transition: background 300ms ease-in-out;
+}
