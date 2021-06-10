5ae35f4d2c9d6e8e713aa405d92f7fad8b615a64
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 15:34:27 -0700

    added skeleton for footer component, export statement

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,1 +1,13 @@
 import { Link, Icon, Flex, Text, Stack } from '@chakra-ui/react';
+
+const Footer = ({ h }) => {
+  <Flex h={h} justifyContent="center" alignItems="center">
+    <Text>
+      Made by
+      <Link href="https://www.google.com">Nick Papadakis</Link>
+    </Text>
+    <Stack></Stack>
+  </Flex>;
+};
+
+export default Footer;
