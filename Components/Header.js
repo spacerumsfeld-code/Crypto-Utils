8260fa9e056d05795d7ab4e4dc22b577316d1def
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 20:42:37 -0700

    added nextLink and more component imports to Header

--- Components/Header.js
+++ Components/Header.js
@@ -1,1 +1,11 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading, Stack, Image, Link } from '@chakra-ui/react';
+
+import NextLink from 'next/link';
+
+const Header = ({ h }) => {
+  <Flex>
+    <Heading>
+      <Link as={NextLink} href="/"></Link>
+    </Heading>
+  </Flex>;
+};
