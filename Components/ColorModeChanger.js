3eeef257def284ca8036e12941caeb48f712a9c0
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 20:59:53 -0700

    added colormodechanger function body and export statement

--- Components/ColorModeChanger.js
+++ Components/ColorModeChanger.js
@@ -1,2 +1,6 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Sunicon, MoonIcon } from '@chakra-ui/icons';
+
+const ColorModeChanger = () => {};
+
+export default ColorModeChanger;
