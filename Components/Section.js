a9c0c853bd301d2c759abd854ffaf578634fc20c
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 20:38:58 -0700

    completed Section component to functional standard, moving to Header

--- Components/Section.js
+++ Components/Section.js
@@ -3,11 +3,28 @@ import NextLink from 'next/link';
 
 const Section = ({ title, href, image }) => (
   <Link as={NextLink} href={href}>
-    <Flex flexDirection="column" alignItems="center">
+    <Flex
+      w="100%"
+      h="100%"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+      padding="1em"
+      border="1px solid white"
+      borderRadius="8px"
+      cursor="pointer"
+      shadow="xl"
+    >
       <Box flex={1}>
         <Image objectFit="contain" src={image} alt={title} />
       </Box>
-      <Heading text-align="center">{title}</Heading>
+      <Heading
+        bgClip="text"
+        bgGradient="linear(to-t, green.200, pink.500)"
+        text-align="center"
+      >
+        {title}
+      </Heading>
     </Flex>
   </Link>
 );
