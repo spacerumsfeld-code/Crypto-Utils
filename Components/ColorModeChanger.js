5c6f0b2784b73a1fd70bc0073a0aaaa6f387a803
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 21:03:00 -0700

    added return to colormodechanger

--- Components/ColorModeChanger.js
+++ Components/ColorModeChanger.js
@@ -3,6 +3,12 @@ import { Sunicon, MoonIcon } from '@chakra-ui/icons';
 
 const ColorModeChanger = () => {
   const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+  const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
+
+  return (
+    <IconButton size="lg" fontSize="lg" />;
+  )
 };
 
 export default ColorModeChanger;
