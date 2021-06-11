01c6e1bbd083d821a94bab9c3dee27bd45b9ccef
Nick Papadakis <nickfin2014@gmail.com>
2021-06-11 12:48:23 -0700

    converted footlink component to typescript

--- Components/FooterLink.tsx
+++ Components/FooterLink.tsx
@@ -1,6 +1,13 @@
 import { Link, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
 
-const FooterLink = ({ icon, href, label }) => (
+type FooterLinkProps = {
+  icon: IconType;
+  href: string;
+  label: string;
+};
+
+const FooterLink = ({ icon, href, label }: FooterLinkProps) => (
   <Link display="inline-block" href={href} aria-label={label} isExternal>
     <Icon as={icon} fontSize="xl" boxSize="1.5em" color="white.100" />
   </Link>
