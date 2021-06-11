27ca55034963bd7eeb86f793060d117a6dff1a03
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 19:59:17 -0700

    added component skeleton for Section Component

--- Components/Section.js
+++ Components/Section.js
@@ -1,6 +1,15 @@
 import { Box, Link, Heading, Image, Flex } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
-const Section = ({ title, href, image }) => {};
+const Section = ({ title, href, image }) => {
+  <Link as={NextLink} href={href}>
+    <Flex>
+      <Box>
+        <Image></Image>
+      </Box>
+      <Heading></Heading>
+    </Flex>
+  </Link>;
+};
 
 export default Section;
