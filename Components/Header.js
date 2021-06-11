b0ec2aed8650acddcd62e6ae92ce1f2d17d079f2
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 20:43:17 -0700

    added height props to Flex in Header

--- Components/Header.js
+++ Components/Header.js
@@ -3,7 +3,7 @@ import { Flex, Heading, Stack, Image, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Header = ({ h }) => {
-  <Flex>
+  <Flex h={h}>
     <Heading>
       <Link as={NextLink} href="/"></Link>
     </Heading>
