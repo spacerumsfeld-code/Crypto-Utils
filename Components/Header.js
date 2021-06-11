30fdd7595a535461b36a382a97754526e054a3b4
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 20:50:47 -0700

    added export statement to header

--- Components/Header.js
+++ Components/Header.js
@@ -3,9 +3,19 @@ import { Flex, Heading, Stack, Image, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Header = ({ h }) => {
+
+//router
+
+  return (
+
+  )
   <Flex h={h}>
     <Heading>
-      <Link as={NextLink} href="/"></Link>
+      <Link as={NextLink} href="/">
+        Crypto Digest
+      </Link>
     </Heading>
   </Flex>;
 };
+
+export default Header;
