34d96a0f6c5c5993ff15375595ad10442ed8b648
Nick Papadakis <nickfin2014@gmail.com>
2021-06-10 21:00:41 -0700

    added useColorMode hook to colormodechanger

--- Components/ColorModeChanger.js
+++ Components/ColorModeChanger.js
@@ -1,6 +1,8 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Sunicon, MoonIcon } from '@chakra-ui/icons';
 
-const ColorModeChanger = () => {};
+const ColorModeChanger = () => {
+  const { toggleColorMode } = useColorMode();
+};
 
 export default ColorModeChanger;
