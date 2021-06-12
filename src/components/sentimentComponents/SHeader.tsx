38609fb2cb6cf0cbec120f7a5dc783e26dec5d78
Nick Papadakis <nickfin2014@gmail.com>
2021-06-12 16:02:20 -0700

    removed border from Sentiment Header

--- src/components/sentimentComponents/SHeader.tsx
+++ src/components/sentimentComponents/SHeader.tsx
@@ -7,12 +7,7 @@ type SHeaderProps = {
 //note: mess with fontSize for header perhaps
 
 const SHeader = ({ asset }: SHeaderProps) => (
-  <Flex
-    flexDirection="column"
-    justifyContent="center"
-    alignItems="center"
-    border="1px solid white"
-  >
+  <Flex flexDirection="column" justifyContent="center" alignItems="center">
     <Heading fontSize="5vw">Sentiment</Heading>
     <Text fontSize={['0.9em', '1em']}>{`${asset} sentiment for the day`}</Text>
   </Flex>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,8 +9,10 @@ import {
   AccordionButton,
   AccordionPanel
 } from '@chakra-ui/react';
+
 import SHeader from '@/sentimentComponents/SHeader';
 import AssetIcon from '@/sentimentComponents/AssetIcon';
+import Summary from '@/sentimentComponents/Summary';
 import PositiveTweets from '@/sentimentComponents/PositiveTweets';
 import NegativeTweets from '@/sentimentComponents/NegativeTweets';
 
@@ -28,15 +30,8 @@ const AboutPage = () => (
     <SHeader asset={'bitcoin'} />
     <AssetIcon asset={'section-image'} />
     <Stack w={['80%', '70%']}>
-      <Box textAlign="center">
-        <Text fontSize={['md', 'lg']}>
-          Sentiment Summary! This is a placeholder paragraph to indicate an
-          example size of a sentiment summary paragraph. Currently, 100 percent
-          of tweets are positive. The overall sentiment is ______ with ____ of
-          tweets being positive and ____ of tweets being negative
-        </Text>
-      </Box>
-      <Accordion allowMultiple defaultIndex={[0]} allowToggle>
+      <Summary />
+      <Accordion allowMultiple allowToggle>
         <AccordionItem>
           <h2>
             <AccordionButton>
