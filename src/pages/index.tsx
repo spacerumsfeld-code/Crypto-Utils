f6e64489c3469b20dad48bb636fad531cf7d2ca3
Nick Papadakis <nickfin2014@gmail.com>
2021-06-13 12:29:43 -0700

    added notes for eventual nextAuth implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import Section from '@/components/Section';
 
 import assets from '@/lib/assets';
 
+//future: when nextAuth is incorporated, create additional filter here to filter different utilies based on if there is a user or not. This will constrain an unregistered visitor to certain utilies displaying in the navbar. Once they log in and go to navigation, they will see the full list of utilies
+
 export default function Home() {
   return (
     <>
