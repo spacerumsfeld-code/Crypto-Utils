3f8fafa61c8f285d9044a36ee3363dd8f6bfb852
Nick Papadakis <nickfin2014@gmail.com>
2021-06-14 17:45:48 -0700

    added props type for summary component

--- src/components/sentimentComponents/Summary.tsx
+++ src/components/sentimentComponents/Summary.tsx
@@ -2,7 +2,15 @@ import { Box, Text } from '@chakra-ui/react';
 
 //add positive and negative to props, bring in utility function that assigns sentiment descriptors based on ratios in those props, render from the result of the util with template literals
 
-const Summary = () => (
+type SummaryProps = {
+  asset: string;
+  positiveCount: number;
+  negativeCount: number;
+};
+
+const;
+
+const Summary = ({ asset, positiveCount, negativeCount }: SummaryProps) => (
   <Box mb={4} textAlign="center">
     <Text fontSize={['md', 'lg']}>
       Sentiment Summary! This is a placeholder paragraph to indicate an example
