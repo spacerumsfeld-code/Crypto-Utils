69deec88bf037c725f047620b602ab8717dcb878
Nick Papadakis <nickfin2014@gmail.com>
2021-06-16 16:02:39 -0700

    adjusted height for container in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,17 +17,22 @@ export default function About() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container
+        h="100%"
         maxW="70%"
         bg={bg}
         shadow="rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px"
       >
         <Flex direction="column" justifyContent="center" alignItems="center">
           <Heading>Welcome to Crypto-Utils</Heading>
-          <Text>Your one-stop shop for digital cryptocurrency utilities</Text>
+          <Text>A Digital Cryptocurrency Utility Platform</Text>
           <Text my={4}>
-            Welcome to Crypt-Utils, an application with all the tools you need!
+            Crypto-Utils is designed to be your one-stop shop for a variety of
+            utilities to get the information you need!
           </Text>
-          <Text></Text>
+          <Text>
+            Crypto-Utils is your one-stop shop for a variety of utilities
+            pertaining to digital cryptocurrencies.{' '}
+          </Text>
           <Text></Text>
         </Flex>
       </Container>
