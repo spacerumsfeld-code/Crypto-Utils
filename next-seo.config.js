cbbae9fed56d34c0459dd0b108a7347b7e2ff60a
Nick Papadakis <nickfin2014@gmail.com>
2021-06-17 13:44:17 -0700

    added openGraph properties to next-seo config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,5 +1,14 @@
 export default {
   title: 'Crypto-Utils',
   description: 'A one-stop shop for cryptocurrency tools',
-  canonical: 'https://www.crypto-utils.app'
+  canonical: 'https://www.crypto-utils.app',
+  openGraph: {
+    profile: {
+      username: 'Nick Papadakis'
+    },
+    type: 'website',
+    locale: 'en',
+    url: 'https://www.crypto-utils.app',
+    site_name: 'Crypto-Utils'
+  }
 };
