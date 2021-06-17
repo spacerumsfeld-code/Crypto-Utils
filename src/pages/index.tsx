15e3eed59b7dff466b25a843dc40ab67691055db
Nick Papadakis <nickfin2014@gmail.com>
2021-06-16 18:13:27 -0700

    added responsive maxWidth to index container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,8 @@ export default function Home() {
       </Head>
       <Container
         h="100%"
-        maxW="70%"
+        p="1rem"
+        maxW={['80%', '70%']}
         bg={bg}
         shadow="rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px"
       >
