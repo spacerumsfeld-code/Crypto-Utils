c123df843db78ba221893168fcfda69470c47a48
Nick Papadakis <nickfin2014@gmail.com>
2021-06-17 13:52:24 -0700

    added SEO import into _app so that SEO settings can apply to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
-// import '@/styles/globals.css';
-import theme from '@/styles/theme';
 import Layout from '@/components/Layout';
+import theme from '@/styles/theme';
+import SEO from '../../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
