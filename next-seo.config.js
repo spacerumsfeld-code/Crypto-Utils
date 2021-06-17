5358ff4cfb37336af7cc01de34a4145e374f04df
Nick Papadakis <nickfin2014@gmail.com>
2021-06-17 13:49:23 -0700

    hosted preview image and added link to it in next-seo openGraph settings

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,6 +9,14 @@ export default {
     type: 'website',
     locale: 'en',
     url: 'https://www.crypto-utils.app',
-    site_name: 'Crypto-Utils'
+    site_name: 'Crypto-Utils',
+    images: [
+      {
+        url: 'https://ibb.co/9TSymBH',
+        width: 200,
+        height: 200,
+        alt: 'Crypto-Utils'
+      }
+    ]
   }
 };
