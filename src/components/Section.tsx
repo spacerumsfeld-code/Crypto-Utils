7acfd5d779656aea27d04455669b27d0f17ade55
Nick Papadakis <nickfin2014@gmail.com>
2021-06-16 18:06:15 -0700

    added container for index page

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -20,9 +20,6 @@ const Section = ({ title, href, image, onClick }: SectionProps) => (
         borderRadius="8px"
         cursor="pointer"
         shadow="rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px"
-        // bgImage="/card-background.svg"
-        // bgPosition="center"
-        // bgSize="100%"
       >
         <Box flex={1}>
           <Image objectFit="contain" src={image} alt={title} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import { SimpleGrid } from '@chakra-ui/react';
+import { Container, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
 import Head from 'next/head';
 
 import Section from '@/components/Section';
 
 import assets from '@/lib/assets';
 
-//future: when nextAuth is incorporated, create additional filter here to filter different utilies based on if there is a user or not. This will constrain an unregistered visitor to certain utilies displaying in the navbar. Once they log in and go to navigation, they will see the full list of utilies
-
 export default function Home() {
+  const bg = useColorModeValue('white', 'gray.800');
   return (
     <>
       <Head>
@@ -15,13 +14,20 @@ export default function Home() {
         <meta name="Home Page" content="Crypto-Utils" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SimpleGrid w="100%" gap={8} minChildWidth="20em" gridAutoRows="1fr">
-        {assets
-          .filter(({ href }) => href !== '/')
-          .map((asset) => (
-            <Section key={asset.title} {...asset} />
-          ))}
-      </SimpleGrid>
+      <Container
+        h="100%"
+        maxW="70%"
+        bg={bg}
+        shadow="rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px"
+      >
+        <SimpleGrid w="100%" gap={8} minChildWidth="10em" gridAutoRows="1fr">
+          {assets
+            .filter(({ href }) => href !== '/')
+            .map((asset) => (
+              <Section key={asset.title} {...asset} />
+            ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
